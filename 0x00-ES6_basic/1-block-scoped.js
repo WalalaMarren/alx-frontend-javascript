d3aa59adcd41af6d909f5315861b12eb999af97c
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-11-27 20:20:17 +0300

    hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,9 @@
+var task = false;
+var task2 = true;
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
-  
     if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
+      var task = true;
+      var task2 = false;
     }
   
     return [task, task2];
