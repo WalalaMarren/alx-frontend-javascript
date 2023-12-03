2261c64c0903786f6d5074214b792b52c0d71462
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-12-03 12:52:32 +0300

    class setter and getter methods

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,25 @@
+class HolbertonCourse{
+    constructor(name, length, students){
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+    get name(){
+        return this.name;
+    }
+    set name(newName){
+        this._name = newName;
+    }
+    get length(){
+        return this.length;
+    }
+    set length(newLength){
+        this._length = newLength;
+    }
+    get students(){
+        return this.students;
+    }
+    set students(newStudent){
+        this._students = newStudent;
+    }
+}
